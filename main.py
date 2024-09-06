5f72b5b0a1fe827863a2eeb0f21ecc48b89500af
Alex <alexander.mykyta@gmail.com>
2024-09-06 22:18:11 +0300

    shuffle added (but have to change click-ability of slider)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter.filedialog import askdirectory
 from PIL import Image
 import os
 import sys
+from random import shuffle
 
 pygame.init()
 mixer.init()
@@ -19,6 +20,7 @@ window: CTk
 volume_slider: CTkSlider
 volume: float = 0
 position_slider: CTkSlider
+shuffle_slider: CTkSlider
 paused: bool = False
 play_button: CTkButton
 add_songs_button: CTkButton
@@ -59,32 +61,58 @@ def position_update(_):
         mixer.music.set_pos(pos)
 
 
-def load_songs():
-    global music_queue, music_listbox, clear_button, add_songs_button
+def shuffle_update(_):
+    global music_listbox, music_queue, shuffle_slider
+    shuffle_value = round(shuffle_slider.get())
+    shuffle_slider.set(shuffle_value)
+    if shuffle_value:
+        if music_listbox.size() > 2:
+            shuffle(music_queue)
+            listbox_update()
+
+
+def clear_listbox():
+    global music_listbox, current_song_index, current_song_label
+    if music_listbox.size():
+        current_song_label.configure(text="BRAGI")
+        current_song_index = 0
+        mixer.music.unload()
+        music_listbox.delete("all")
+
+def listbox_update():
+    global music_queue, music_listbox, clear_button, add_songs_button, current_song_index, current_song_label
 
     clear_button.configure(state="disabled")
     add_songs_button.configure(state="disabled")
 
-    first_song = True if len(music_queue) < 1 else False
+    if len(music_queue) > 0:
+        clear_listbox()
+
+    for song in music_queue:
+        music_listbox.insert(END, song.split("\\")[-1][:-4])
+
+    if music_listbox.size() > 0:
+        music_listbox.select(0)
+
+    clear_button.configure(state="normal")
+    add_songs_button.configure(state="normal")
+
+
+def load_songs():
+    global music_queue, shuffle_slider
 
     music_directory = askdirectory()
     if not music_directory:
-        clear_button.configure(state="normal")
-        add_songs_button.configure(state="normal")
         return
 
     for root, _, files in os.walk(music_directory):
         for file in files:
             if file.endswith(".mp3") or file.endswith(".ogg") or file.endswith(".wav"):
                 music_queue.append(os.path.join(root, file))
-                music_listbox.insert(END, file[:-4])
 
-            if first_song:
-                first_song = False
-                music_listbox.select(0)
-
-    clear_button.configure(state="normal")
-    add_songs_button.configure(state="normal")
+    if shuffle_slider.get():
+        shuffle(music_queue)
+    listbox_update()
 
 
 def clear_playlist():
@@ -93,11 +121,8 @@ def clear_playlist():
     add_songs_button.configure(state="disabled")
     clear_button.configure(state="disabled")
 
-    current_song_label.configure(text="BRAGI")
-    current_song_index = 0
     music_queue.clear()
-    mixer.music.unload()
-    music_listbox.delete("all")
+    clear_listbox()
 
     add_songs_button.configure(state="normal")
     clear_button.configure(state="normal")
@@ -147,7 +172,8 @@ def play(mode: str = "default"):
 
 
 def gui_startup():
-    global volume_slider, position_slider, play_button, music_listbox, current_song_label, add_songs_button, clear_button
+    global volume_slider, position_slider, play_button, music_listbox, current_song_label, add_songs_button, \
+        clear_button, shuffle_slider
 
     bragi_image = CTkImage(Image.open(resource_path("bragi.png")),
                            size=(250, 250))
@@ -195,7 +221,7 @@ def gui_startup():
     current_song_image_image.pack(pady=0)
 
     command_frame = CTkFrame(master=ctk, fg_color=color_soft_beige)
-    command_frame.pack(fill="x", padx=120, pady=20)
+    command_frame.pack(fill="x", padx=120, pady=10)
 
     CTkButton(master=command_frame, text="<", command=lambda: play("prev"), fg_color=color_golden_yellow,
               hover_color=color_deep_red, width=70, corner_radius=10, font=font,
@@ -210,17 +236,40 @@ def gui_startup():
               hover_color=color_deep_red, width=70, corner_radius=10, font=font,
               text_color=color_dark_charcoal).pack(side="left", padx=5)
 
-    position_slider = CTkSlider(master=ctk, orientation="horizontal", from_=0, to=100,
-                                fg_color=color_dark_charcoal, progress_color=color_golden_yellow,
-                                button_color=color_golden_yellow, hover=False, width=230, border_width=0)
-    position_slider.bind("<ButtonRelease-1>", position_update)
-    position_slider.pack(pady=0)
+    sliders_frame = CTkFrame(master=ctk, fg_color=color_soft_beige)
+    sliders_frame.pack(fill="x", padx=125, pady=0)
 
-    volume_slider = CTkSlider(master=ctk, orientation="horizontal", from_=0, to=100, command=volume_update,
-                              fg_color=color_dark_charcoal, progress_color=color_golden_yellow, width=230,
-                              button_color=color_golden_yellow, hover=False, border_width=0)
-    volume_slider.set(50)
-    volume_slider.pack(pady=5)
+    sf_0 = CTkFrame(master=sliders_frame, fg_color=color_soft_beige)
+    sf_0.pack(fill="x", padx=0, pady=0)
+
+    CTkLabel(master=sf_0, font=font, text_color=color_dark_charcoal, width=70,
+             text="Position").pack(side="left", pady=0, padx=0)
+    position_slider = CTkSlider(master=sf_0, orientation="horizontal", from_=0, to=100,
+                                fg_color=color_dark_charcoal, progress_color=color_golden_yellow,
+                                button_color=color_golden_yellow, hover=False, width=150, border_width=0)
+    position_slider.bind("<ButtonRelease-1>", position_update)
+    position_slider.pack(side="right", pady=0, padx=0)
+
+    sf_1 = CTkFrame(master=sliders_frame, fg_color=color_soft_beige)
+    sf_1.pack(fill="x", padx=0, pady=0)
+
+    CTkLabel(master=sf_1, font=font, text_color=color_dark_charcoal, width=70,
+             text="Volume").pack(side="left", pady=0, padx=0)
+    volume_slider = CTkSlider(master=sf_1, orientation="horizontal", from_=0, to=100, command=volume_update,
+                                fg_color=color_dark_charcoal, progress_color=color_golden_yellow,
+                                button_color=color_golden_yellow, hover=False, width=150, border_width=0)
+    volume_slider.pack(side="right", pady=0, padx=0)
+
+    sf_2 = CTkFrame(master=sliders_frame, fg_color=color_soft_beige)
+    sf_2.pack(fill="x", padx=0, pady=0)
+
+    CTkLabel(master=sf_2, font=font, text_color=color_dark_charcoal, width=70,
+             text="Shuffle").pack(side="left", pady=0, padx=0)
+    shuffle_slider = CTkSlider(master=sf_2, orientation="horizontal", from_=0, to=1, command=shuffle_update,
+                              fg_color=color_dark_charcoal, progress_color=color_dark_charcoal,
+                              button_color=color_golden_yellow, hover=False, width=150, border_width=0)
+    shuffle_slider.set(0)
+    shuffle_slider.pack(side="right", pady=0, padx=0)
 
     end_check(ctk)
     ctk.mainloop()
